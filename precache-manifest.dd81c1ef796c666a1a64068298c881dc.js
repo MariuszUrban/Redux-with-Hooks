14338db574233e715d9e31a0d8912cf494b7249c
Mariusz <urban.mariusz@icloud.com>
2021-02-08 20:54:36 +0100

    Updates

---
+++ precache-manifest.dd81c1ef796c666a1a64068298c881dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4c4c4b9b70aefff7627",
+    "url": "/Todo-Redux-with-Hooks/static/css/main.f20d97f0.chunk.css"
+  },
+  {
+    "revision": "c4c4c4b9b70aefff7627",
+    "url": "/Todo-Redux-with-Hooks/static/js/main.61125c31.chunk.js"
+  },
+  {
+    "revision": "15f924a03edfa14948cb",
+    "url": "/Todo-Redux-with-Hooks/static/js/runtime~main.16da74e6.js"
+  },
+  {
+    "revision": "0de1fd1d4837ea4f1a12",
+    "url": "/Todo-Redux-with-Hooks/static/js/2.def10afd.chunk.js"
+  },
+  {
+    "revision": "27dfc467b844fcb7a6ff5fd0a7e96651",
+    "url": "/Todo-Redux-with-Hooks/index.html"
+  }
+];
